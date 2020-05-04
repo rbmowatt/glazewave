f90606e6b364024d2aef5bd5d66e59f9bc97d870
Richard Mowatt <richmowatt@gmail.com>
2020-05-04 14:11:43 -0400

    adding libraries

--- backend/app/api/user.js
+++ backend/app/api/user.js
@@ -1,72 +1,87 @@
-const { Router }=require('express');
-
-var AWS = require("aws-sdk");
+const { Router } = require('express');
+const uuid = require('uuid');
+const shortid = require('shortid');
+//shortid.characters('0123456789');
+ 
+const AWS = require("aws-sdk");
 AWS.config.update({
     region: "eu-west-2",
     endpoint: "http://localhost:8000"
-  });
+});
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const router = new Router();
 
-  router .get('/', function (req, res) {
-  var params = {
-      TableName: "users",
-      ProjectionExpression: "#id, #email, #first_name, #last_name, #role",
-      ExpressionAttributeNames: {
-          "#id": "id",
-          "#first_name": "first_name",
-          "#last_name": "last_name",
-          "#email": "email",
-          "#role": "role"
-      }
-  };
-  console.log("Scanning Users table.");
-  docClient.scan(params, onScan);
-  function onScan(err, data) {
-      if (err) {
-          console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-      } else {
-          res.send(data)
-          // print all the Users
-          console.log("Scan succeeded.");
-          data.Items.forEach(function(car) {
-             console.log(car.id, car.type, car.name)
-          });
-  if (typeof data.LastEvaluatedKey != "undefined") {
-              console.log("Scanning for more...");
-              params.ExclusiveStartKey = data.LastEvaluatedKey;
-              docClient.scan(params, onScan);
-          }
-      }
+router.get('/', function (req, res) {
+    const params = {
+        TableName: "users",
+        ProjectionExpression: "#id, #email, #first_name, #last_name, #role",
+        ExpressionAttributeNames: {
+            "#id": "id",
+            "#first_name": "first_name",
+            "#last_name": "last_name",
+            "#email": "email",
+            "#role": "role"
+        }
+    };
+    docClient.scan(params, onScan);
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            res.send(data)
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
+        }
     }
-  })
-  
-  router .get('/:id', function (req, res) {
-      var userId = parseInt(req.params.id);
-        console.log(req.url)
-        console.log(userId)
-      var params = {
-            TableName : "users",
-            KeyConditionExpression: "#id = :id",
-            ExpressionAttributeNames:{
-                "#id": "id"
-            },
-            ExpressionAttributeValues: {
-                ":id": userId
+})
+
+router.get('/:id', function (req, res) {
+    const userId = parseInt(req.params.id);
+    const params = {
+        TableName: "users",
+        KeyConditionExpression: "#id = :id",
+        ExpressionAttributeNames: {
+            "#id": "id"
+        },
+        ExpressionAttributeValues: {
+            ":id": userId
+        }
+    };
+    docClient.query(params, function (err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            res.send(data.Items)
+        }
+    });
+});
+
+router.post('/', function (req, res) {
+        const params = {
+            TableName: "users",
+            Item: {
+                "id": Date.now(),
+                "email": req.body.email || null,
+                "first_name": req.body.first_name || null,
+                "last_name": req.body.last_name || null,
+                "role": req.body.role || null,
+                "gender": req.body.gender || null
             }
         };
-      docClient.query(params, function(err, data) {
-          if (err) {
-              console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-          } else {
-              console.log("Query succeeded.");
-              res.send(data.Items)
-              data.Items.forEach(function(user) {
-                  console.log(user.id, user.first_name, user.last_name);
-              });
-          }
-      });
-      });
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.error("Unable to add User", req.body, ". Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                res.send(data);
+                console.log("PutItem succeeded:", data.Items);
+            }
+        });
+
+});
 
 module.exports = router;
