af4a019be53d28963d4b444646f3c03bee7ce40f
Richard Mowatt <richmowatt@gmail.com>
2020-05-06 17:09:56 -0400

    maintaining persistence after refresh

--- frontend/src/actions/session.js
+++ frontend/src/actions/session.js
@@ -1,23 +1,30 @@
 import { CLEAR_SESSION, SET_SESSION } from './types'
 import cognitoUtils from '../lib/utils/cognito'
 import Cookie from "js-cookie"
+import { hasSession } from '../lib/utils/session';
+
 
 export const clearSession = () => ({
   type: CLEAR_SESSION
 })
 
+export function initSession()
+{
+  return function (dispatch) {
+  console.log('called init session');
+    let session = hasSession();
+    if(session)
+    {
+      cognitoUtils.getCognitoSession();
+      dispatch({ type: SET_SESSION, session })
+    }
+  return false;
+  }
+}
+
 // Initialise the Cognito sesson from a callback href
 export function initSessionFromCallbackURI (callbackHref) {
   return function (dispatch) {
-    /** 
-    if(Cookie.get("x-token"))
-    {
-      const session = JSON.parse(Cookie.get("x-token"));
-      console.log('session', session);
-      dispatch({ type: SET_SESSION, session })
-      return;
-    }
-    **/
     return cognitoUtils.parseCognitoWebResponse(callbackHref) // parse the callback URL
       .then(() => cognitoUtils.getCognitoSession()) // get a new session
       .then((session) => {
