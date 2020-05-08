6abb340ca10ef3a68f3a5cec0faa0e687c79d2e1
Jimmy Wilson <jimmywilson@Jimmys-iMac.local>
2020-05-08 18:02:43 -0400

    update client url

--- backend/app/index.js
+++ backend/app/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const userRouter = require('./api/user');
 const recipeRouter = require('./api/recipe');
-const appConfig = require('./config');
+const appConfig = require('./config/app');
 const cognitoConfig = require('./config/cognito');
 const cognitoAuth = require('./lib/cognitoAuth');
 const cognitoAuthMiddleware = cognitoAuth.getVerifyMiddleware()
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({'origin': [cognitoConfig.signoutUri, appConfig.uiHost ]}));
+app.use(cors({'origin': [cognitoConfig.signoutUri, appConfig.clientUrl ]}));
 
 app.use('/user', cognitoAuthMiddleware, userRouter);
 app.use('/recipe', recipeRouter);
