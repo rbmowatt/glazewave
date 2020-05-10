3d471da025ed26e9033d2e01323ea7da2450167a
Jimmy Wilson <jimmywilson@Jimmys-iMac.local>
2020-05-10 10:49:08 -0400

    adding build on local

--- frontend/build/precache-manifest.fc06c3220e4d2ff4d9d37022fe6e449a.js
+++ frontend/build/precache-manifest.fc06c3220e4d2ff4d9d37022fe6e449a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc35b6da9763ad725d7ad6ab780e124c",
+    "revision": "d4d679f34a03924587eb6e97c5543630",
     "url": "/index.html"
   },
   {
-    "revision": "b06dc64a531836177c77",
+    "revision": "984e1db0677ff2e6dc31",
     "url": "/static/css/2.ee241de9.chunk.css"
   },
   {
-    "revision": "866904794e16a12a1553",
+    "revision": "859e5d77381374290e67",
     "url": "/static/css/main.fad54ebb.chunk.css"
   },
   {
-    "revision": "b06dc64a531836177c77",
-    "url": "/static/js/2.b1de66f9.chunk.js"
+    "revision": "984e1db0677ff2e6dc31",
+    "url": "/static/js/2.341fc22b.chunk.js"
   },
   {
     "revision": "185ef6fe98b3fd6cb5ad57142af23dda",
-    "url": "/static/js/2.b1de66f9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.341fc22b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "866904794e16a12a1553",
-    "url": "/static/js/main.0b9d0079.chunk.js"
+    "revision": "859e5d77381374290e67",
+    "url": "/static/js/main.3dbf6c93.chunk.js"
   },
   {
     "revision": "c3c5018b854d64cdc7f2",
