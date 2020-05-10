49c741ef49d17d9f446017732368a4f4dc2c33b1
Richard Mowatt <richmowatt@gmail.com>
2020-05-10 10:40:40 -0400

    adding build on local

---
+++ frontend/build/precache-manifest.7c2064874f52d06cb7c3d1f807ef6778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc35b6da9763ad725d7ad6ab780e124c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06dc64a531836177c77",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "866904794e16a12a1553",
+    "url": "/static/css/main.fad54ebb.chunk.css"
+  },
+  {
+    "revision": "b06dc64a531836177c77",
+    "url": "/static/js/2.b1de66f9.chunk.js"
+  },
+  {
+    "revision": "185ef6fe98b3fd6cb5ad57142af23dda",
+    "url": "/static/js/2.b1de66f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866904794e16a12a1553",
+    "url": "/static/js/main.0b9d0079.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
