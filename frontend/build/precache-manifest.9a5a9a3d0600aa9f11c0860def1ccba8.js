0e4ad4b3f38ace3f091d6258072ff425ae13565b
Jimmy Wilson <jimmywilson@Jimmys-iMac.local>
2020-05-13 10:48:21 -0400

    fixed phone number prefix

--- frontend/build/precache-manifest.9a5a9a3d0600aa9f11c0860def1ccba8.js
+++ frontend/build/precache-manifest.9a5a9a3d0600aa9f11c0860def1ccba8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adde0e5a50431715fcf1ffe02d67588c",
+    "revision": "be61daac558db7c831ac706abdb89a3b",
     "url": "/index.html"
   },
   {
-    "revision": "4ff23c5c00a71947fd5f",
-    "url": "/static/css/main.47d3d900.chunk.css"
+    "revision": "c4eed411a8881dec5a84",
+    "url": "/static/css/main.39d085b0.chunk.css"
   },
   {
     "revision": "ac4af5e247bb3571b7ae",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f24e956a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4ff23c5c00a71947fd5f",
-    "url": "/static/js/main.ca1aadc6.chunk.js"
+    "revision": "c4eed411a8881dec5a84",
+    "url": "/static/js/main.aa0af5dd.chunk.js"
   },
   {
     "revision": "c3c5018b854d64cdc7f2",
--- frontend/src/components/user/UserForm.js
+++ frontend/src/components/user/UserForm.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 export const UserForm = props =>{
+    const addPhonePrefix = e =>
+    {   
+        e.persist();
+        const validKeys = [8, 37, 39];
+        if( validKeys.indexOf(e.keyCode) === -1 && (e.currentTarget.value.length > 11 || (!Number.isInteger(parseInt(e.key)))))
+        {
+            e.preventDefault();
+            return;
+        }
+        let currentValue = e.currentTarget.value;
+        let target = e.currentTarget;
+        let key = e.key;
+        setTimeout(function () {
+            if(target.value != '' && currentValue.indexOf('+1') !== 0) {
+                target.value = '+1' + key;
+            } 
+        }, 1);
+    }
     return (
         <form className="row" id={"create-post-form"} onSubmit={props.processFormSubmission} noValidate={true}>
         <div className="form-group col-md-12">
@@ -18,7 +36,7 @@ export const UserForm = props =>{
 
             <div className="form-group col-md-12">
                 <label htmlFor="phone"> Phone </label>
-                <input type="text" id="phone_number" defaultValue={(props.edit && props.user.phone_number) || ''} onChange={(e) => props.handleInputChanges(e)} name="phone_number" className="form-control" placeholder="+[d]1234567890" />
+                <input type="text" id="phone_number" defaultValue={(props.edit && props.user.phone_number) || ''} onKeyDown={(e)=> addPhonePrefix(e)} onChange={(e) => props.handleInputChanges(e)} name="phone_number" className="form-control" placeholder="+[d]1234567890" />
             </div>
 
             <div className="form-group col-md-4 pull-right">
