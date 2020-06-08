54c5829ef0d983e4636715bf5a41d6a80c759eb1
Jimmy Wilson <jimmywilson@Jimmys-iMac.local>
2020-06-08 09:27:12 -0400

    update sequalize config

--- backend/app/config/sequelize.js
+++ backend/app/config/sequelize.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports =  {
-  'config': path.join(__dirname, '../config/mysql.json'),
+  'config': path.join(__dirname, '../config/mysql.js'),
   'migrations-path': path.join(__dirname, '../migrations'),
   'seeders-path': path.join(__dirname, 'seeders'),
   'models-path': path.join(__dirname, './../models'),
