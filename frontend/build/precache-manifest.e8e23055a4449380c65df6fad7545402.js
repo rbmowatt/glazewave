ff63243a495e6ca45f3782ac7072776109e9db9c
Jimmy Wilson <jimmywilson@Jimmys-iMac.local>
2020-07-05 12:21:15 -0400

    new build

---
+++ frontend/build/precache-manifest.e8e23055a4449380c65df6fad7545402.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46343dc1ef050cf6ce67a61c640eb821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ac49038b0116f479dd6",
+    "url": "/static/css/2.8a5e2d33.chunk.css"
+  },
+  {
+    "revision": "66781a21be8a510bee46",
+    "url": "/static/css/main.54cf6b4a.chunk.css"
+  },
+  {
+    "revision": "6ac49038b0116f479dd6",
+    "url": "/static/js/2.49ab4bc3.chunk.js"
+  },
+  {
+    "revision": "64ff43fc5c124cb2d0ce309d960f48d7",
+    "url": "/static/js/2.49ab4bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66781a21be8a510bee46",
+    "url": "/static/js/main.b03776f5.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
