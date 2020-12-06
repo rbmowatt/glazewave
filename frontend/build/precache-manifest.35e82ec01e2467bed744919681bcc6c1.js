077a08805788f68db6a024bab06ed0d427759e04
Richard Mowatt <richmowatt@gmail.com>
2020-12-06 12:35:32 -0500

    new build

---
+++ frontend/build/precache-manifest.35e82ec01e2467bed744919681bcc6c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc2dc38092503ab11107dda2af8e64d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2efb37fe28bdc707d51",
+    "url": "/static/css/2.f6d0f199.chunk.css"
+  },
+  {
+    "revision": "4d4e4c65d646ed7ff54b",
+    "url": "/static/css/main.39497cb5.chunk.css"
+  },
+  {
+    "revision": "f2efb37fe28bdc707d51",
+    "url": "/static/js/2.bcd93f0c.chunk.js"
+  },
+  {
+    "revision": "64ff43fc5c124cb2d0ce309d960f48d7",
+    "url": "/static/js/2.bcd93f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4e4c65d646ed7ff54b",
+    "url": "/static/js/main.1957efd5.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
