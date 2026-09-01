bbfd0f03bddf31cd376ffde80a9fefb12c56b94d
Mo Who? <rmowatt@healthrecoverysolutions.com>
2026-09-01 15:52:44 -0700

    return record from update, fix upsert, fix typo, remove no-op, fix implicit global

--- backend/app/services/SessionService.js
+++ backend/app/services/SessionService.js
@@ -6,38 +6,38 @@ const LocationService = require('./LocationService');
 const SessionDataModel = db.SessionData;
 const GooglePlaces = require('node-googleplaces');
 const googleConfig  = require('./../config/google')
-let algolaiIndex = 'user_sessions';
+let algoliaIndex = 'user_sessions';
 
 
 class SessionService  extends BaseService {
 
     constructor(){
-        super(BaseModel, algolaiIndex);
+        super(BaseModel, algoliaIndex);
     }
 
     async create(params, callback = null)
     {
         if(params.location_id)
         {
-           const places = new GooglePlaces(googleConfig.MAPS_KEY);
+            const places = new GooglePlaces(googleConfig.MAPS_KEY);
             return new Promise( (resolve, reject)=>{ places.details({placeid : params.location_id})
-            .then(data=>{
-                let loc = {
-                    id : params.location_id,
-                    name : data.body.result.name,
-                    formatted_address : data.body.result.formatted_address,
-                    lat : data.body.result.geometry.location.lat,
-                    lng : data.body.result.geometry.location.lng,
-                    vicinity : data.body.result.vicinity,
-                    url : data.body.result.url 
-                }
-                LocationService.make().upsert(loc)
-                .then( data=>
-                    {
-                    resolve(super.create(params, callback));
+                .then(data=>{
+                    let loc = {
+                        id : params.location_id,
+                        name : data.body.result.name,
+                        formatted_address : data.body.result.formatted_address,
+                        lat : data.body.result.geometry.location.lat,
+                        lng : data.body.result.geometry.location.lng,
+                        vicinity : data.body.result.vicinity,
+                        url : data.body.result.url
                     }
-                )
-            })})
+                    LocationService.make().upsert(loc)
+                        .then( data=>
+                            {
+                                resolve(super.create(params, callback));
+                            }
+                        )
+                })})
         } else {
             return super.create(params, callback);
         }
@@ -52,25 +52,25 @@ class SessionService  extends BaseService {
     {
         if(params.location_id)
         {
-           const places = new GooglePlaces(googleConfig.MAPS_KEY);
+            const places = new GooglePlaces(googleConfig.MAPS_KEY);
             return new Promise( (resolve, reject)=>{ places.details({placeid : params.location_id})
-            .then(data=>{
-                let loc = {
-                    id : params.location_id,
-                    name : data.body.result.name,
-                    formatted_address : data.body.result.formatted_address,
-                    lat : data.body.result.geometry.location.lat,
-                    lng : data.body.result.geometry.location.lng,
-                    vicinity : data.body.result.vicinity,
-                    url : data.body.result.url 
-                }
-                LocationService.make().upsert(loc)
-                .then( data=>
-                    {
-                    resolve(super.update(id, params, callback));
+                .then(data=>{
+                    let loc = {
+                        id : params.location_id,
+                        name : data.body.result.name,
+                        formatted_address : data.body.result.formatted_address,
+                        lat : data.body.result.geometry.location.lat,
+                        lng : data.body.result.geometry.location.lng,
+                        vicinity : data.body.result.vicinity,
+                        url : data.body.result.url
                     }
-                )
-            })})
+                    LocationService.make().upsert(loc)
+                        .then( data=>
+                            {
+                                resolve(super.update(id, params, callback));
+                            }
+                        )
+                })})
         } else {
             return super.update(id, params, callback);
         }
