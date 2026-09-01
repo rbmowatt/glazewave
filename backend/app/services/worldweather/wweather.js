56ab986b2cd4e93decffb5bee3038fdf9c752643
Mo Who? <rmowatt@healthrecoverysolutions.com>
2026-09-01 15:42:11 -0700

    Fix top 5 code quality issues

--- backend/app/services/worldweather/wweather.js
+++
@@ -1,8 +0,0 @@
-var WWO = require('worldweatheronline-api');
-
-var client = WWO.createClient({
-    key: process.env.wwo_key,
-    responseType: 'json',
-    subscription: 'premium',
-    locale: 'EN'
-});
