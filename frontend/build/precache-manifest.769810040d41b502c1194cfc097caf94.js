57ef9db6d47ccfb8204a945b5cbd5e1026e47489
Mo Who? <rmowatt@healthrecoverysolutions.com>
2026-09-03 17:41:23 -0700

    update build

---
+++ frontend/build/precache-manifest.769810040d41b502c1194cfc097caf94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df08cf3af15d95aa9779e6c1e0f13620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc976d2b2d4c8644260",
+    "url": "/static/css/2.b511c1fb.chunk.css"
+  },
+  {
+    "revision": "29502f10f4a33e7f540a",
+    "url": "/static/css/main.85e7bd81.chunk.css"
+  },
+  {
+    "revision": "cfc976d2b2d4c8644260",
+    "url": "/static/js/2.4dba2cdf.chunk.js"
+  },
+  {
+    "revision": "ca2efa62272350f91fb0ab37e12542f1",
+    "url": "/static/js/2.4dba2cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29502f10f4a33e7f540a",
+    "url": "/static/js/main.22a13358.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
